c18db2f0efcfa77136ee41e6345fd2273597fc05
Alex <alex.dermenji@space01.co.uk>
2022-12-27 20:05:15 +0000

    added hero section

---
+++ components/home-page/hero.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classes from "./hero.module.css";
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          priority={true}
+          src="/images/site/alex.jpg"
+          alt="An image showing Alex"
+          width={500}
+          height={500}
+        />
+      </div>
+      <h1>Hi, I am Alex</h1>
+      <p>This blog is about coding books, especially about web development.</p>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "../components/home-page/hero";
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero />
+      {/* <FeaturedPosts posts={posts} /> */}
+    </>
+  );
+};
+
+export default HomePage;
